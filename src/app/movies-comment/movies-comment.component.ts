7a35a77859ebc31f88db4fbb195e2810ebb86564
Quentin Robert <quentin.robert@epita.fr>
2025-04-07 12:06:48 +0200

    feat(review): Create review card

--- src/app/movies-comment/movies-comment.component.ts
+++ src/app/movies-comment/movies-comment.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {DatePipe} from "@angular/common";
+import {Review} from "../models/review";
 
 @Component({
   selector: 'app-movies-comment',
   standalone: true,
-  imports: [],
+  imports: [
+    DatePipe
+  ],
   templateUrl: './movies-comment.component.html',
   styleUrl: './movies-comment.component.scss'
 })
 export class MoviesCommentComponent {
-
+  @Input({required: true}) review!: Review;
 }
