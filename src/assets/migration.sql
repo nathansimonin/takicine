947ffb7218d7500a2c735698241738f122475248
Thomas Martin-Chemla <thomas.martin-chemla@epita.fr>
2025-04-07 15:07:06 +0200

    Feat: add migration file for more movie entries

---
+++ src/assets/migration.sql
@@ -0,0 +1,178 @@
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Paul', 'Harrohide', 20, 'pharrohide@gmail.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Harry', 'Covert', 25, 'harryCoco32@laposte.net', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Alain', 'Posteur', 28, 'alainposteur@outlook.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Elvire', 'Debord', 45, 'eldebord@gmail.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Laurent', 'Barre', 41, 'lolobarre@gmail.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Homer', 'Cipourtoux', 28, 'homerpassimpson@outlook.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Gaston', 'Laplouz', 67, 'gagalegogo@yahoo.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Gisèle', 'Detable', 39, 'gizoudetable@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Thomas', 'Ouaque', 29, 'touque@laposte.net', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Alain', 'Verse', 28, 'alainverse@outlook.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Camille', 'Onette', 19, 'camilleO@yahoo.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Daisy', 'Rable', 56, 'ddrable@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Gérard', 'Menfaim', 34, 'ggdu94@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Guy', 'Yiotine', 43, 'aiecacoupelatete@gmail.com', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Jean', 'Bonbeur', 63, 'sandwichfrancais@outlook.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Laure', 'Durre', 51, 'lolopascool@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Lara', 'Tatouille', 29, 'yesdeslegumes@laposte.net', 2);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Lucie', 'Fer', 46, 'duferpasquedanslesepinards@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Nicolas', 'Nihorangina', 25, 'quedeleau@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Paul', 'Hémique', 27, 'nonnonnon@yahoo.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Phil', 'Trahuile', 45, 'PhilTT@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Pierre', 'Ponce', 60, 'toutdoux@gmail.com', 0);
+INSERT INTO app_user (first_name, last_name, age, email, points)
+VALUES ('Sylvie', 'Cékilépamore', 60, 'ouf@outlook.com', 2);
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Titanic', 'James Cameron', 2.63, '1998-01-07',
+        'En 1997, lépave du Titanic est lobjet dune exploration fiévreuse, menée par des chercheurs de trésor en quête dun diamant bleu qui se trouvait à bord. Frappée par un reportage télévisé, une des rescapées du naufrage, âgée de 102 ans, Rose DeWitt, se rend sur place et évoque ses souvenirs.',
+        'image_film_1.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Avatar', 'James Cameron', 4.2, '2011-11-02',
+        'Se déroulant plus d’une décennie après les événements relatés dans le premier film, AVATAR : LA VOIE DE L’EAU raconte l''histoire des membres de la famille Sully (Jake, Neytiri et leurs enfants), les épreuves auxquelles ils sont confrontés, les chemins qu’ils doivent emprunter pour se protéger les uns les autres, les batailles qu’ils doivent mener pour rester en vie et les tragédies qu''ils endurent.',
+        'image_film_2.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Dirty Dancing', 'Emile Ardolino', 4.25, '1087-12-23',
+        'Dans les années soixante, Bébé passe des vacances familiales ennuyeuses jusqu''au jour où elle apprend que des animateurs du village estival forment une troupe de danse. Pour cette jeune fille sage, cest le début de l''émancipation à travers l''apprentissage du dirty dancing',
+        'image_film_3.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Eiffel', 'Martin Bourboulon', 4.67, '2021-10-13',
+        'Venant tout juste de terminer sa collaboration sur la Statue de la Liberté, Gustave Eiffel est au sommet de sa carrière. Le gouvernement français veut qu''il crée quelque chose de spectaculaire pour l''Exposition Universelle de 1889 à Paris, mais Eiffel ne s''intéresse qu''au projet de métropolitain. Tout bascule le jour où il recroise son amour de jeunesse. Leur relation interdite l''inspire à changer l''horizon de Paris pour toujours.',
+        'image_film_4.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Les Blues Brothers', 'John Landis', 3.6, '1980-11-07',
+        'Jake et Elwood Blues veulent reformer leur groupe de rhythm''n''blues et donner des concerts afin de gagner les 5000 dollars nécessaires à la survie de l''orphelinat de leur enfance, menacé de fermeture par le fisc. Leur projet s''annonce difficile à réaliser. Poursuivis par la police, une bande de néonazis, des musiciens et une femme mystérieuse, animée d''une rancune tenace, les Blues Brothers se trouvent mêlés à des aventures plus rocambolesques les unes que les autres.',
+        'image_film_5.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Raieponce', 'Nathan Greno & Byron Howard', 2.8, '2010-12-01',
+        'Lorsque Flynn Rider, le bandit le plus recherché du royaume, se réfugie dans une mystérieuse tour, il se retrouve pris en otage par Raiponce, une belle et téméraire jeune fille à l''impressionnante chevelure de 20 mètres de long, gardée prisonnière par Mère Gothel. L''étonnante geôlière de Flynn cherche un moyen de sortir de cette tour où elle est enfermée depuis des années. Elle passe alors un accord avec le séduisant brigand.',
+        'image_film_6.jpg');
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Inception', 'Christopher Nolan', 4.6, '2010-07-21',
+        'Dom Cobb est un voleur d’un genre particulier : il s’introduit dans les rêves pour dérober les secrets les plus précieux. Un ultime contrat pourrait lui rendre sa liberté, mais il devra réussir l’impossible : l’implantation d’une idée.',
+        'image_film_8.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Le Seigneur des Anneaux : La Communauté de l’Anneau', 'Peter Jackson', 5, '2001-12-19',
+        'Un jeune hobbit nommé Frodon hérite d’un anneau aux pouvoirs terrifiants. Il entreprend un périple périlleux pour le détruire dans les flammes de la Montagne du Destin, accompagné d’une communauté de héros.',
+        'image_film_9.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('The Dark Knight', 'Christopher Nolan', 4.8, '2008-08-13',
+        'Batman, le chevalier noir de Gotham, doit affronter un génie du crime imprévisible : le Joker. Une guerre psychologique s’engage, mettant en péril la ville et les idéaux du justicier masqué.',
+        'image_film_10.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Interstellar', 'Christopher Nolan', 4.7, '2014-11-05',
+        'Dans un futur proche, la Terre se meurt. Une équipe d’astronautes est envoyée à travers un trou de ver à la recherche d’une nouvelle planète habitable, défiant le temps et l’espace.',
+        'image_film_11.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Forrest Gump', 'Robert Zemeckis', 4.6, '1994-10-05',
+        'Forrest, un homme simple au grand cœur, traverse les décennies et les événements majeurs de l’histoire américaine, sans jamais se départir de son innocence et de sa gentillesse.',
+        'image_film_12.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Gladiator', 'Ridley Scott', 4.5, '2000-06-20',
+        'Maximus, général romain trahi et réduit en esclavage, devient gladiateur pour venger sa famille et défier l’empereur Corrompus, dans l’arène comme dans l’âme.',
+        'image_film_13.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('La La Land', 'Damien Chazelle', 4.3, '2017-01-25',
+        'À Los Angeles, une actrice en devenir et un musicien passionné vivent une histoire d’amour entre rêves, succès et désillusions, portée par le jazz et les couleurs du cinéma classique.',
+        'image_film_14.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Parasite', 'Bong Joon-ho', 4.6, '2019-06-05',
+        'Une famille pauvre s’immisce peu à peu dans la vie d’une famille riche. Entre satire sociale et tension psychologique, les masques tombent et la vérité éclate de manière brutale.',
+        'image_film_15.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Joker', 'Todd Phillips', 4.4, '2019-10-09',
+        'Arthur Fleck, clown à temps partiel et humoriste raté, sombre peu à peu dans la folie au cœur d’une société indifférente. Il deviendra une figure aussi inquiétante qu’iconique.',
+        'image_film_16.jpg');
+
+INSERT INTO movie (title, director, rate, release_date, synopsis, image)
+VALUES ('Le Fabuleux Destin d’Amélie Poulain', 'Jean-Pierre Jeunet', 4.5, '2001-04-25',
+        'Amélie, jeune serveuse dans un café de Montmartre, décide de faire le bien autour d’elle. Sa vie bascule lorsqu’elle tombe amoureuse d’un inconnu collectionneur de photos d’identité.',
+        'image_film_17.jpg');
+
+
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (2, 1, 4, 'Palpitant', '2004-10-02');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (5, 1, 3, 'Plutôt cool', '2000-12-06');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (3, 2, 5, 'Incroyable, meilleur movie de ma vie', '2018-05-10');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (4, 1, 4, 'Génial', '2005-09-03');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (2, 3, 4, 'TROP BIEN', '2004-09-07');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (2, 2, 4, 'Très captivant', '2017-09-13');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (3, 1, 3, 'Assez bien mais movie un peu lent', '2004-08-29');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (6, 1, 1, 'Pas très intéressant', '2021-09-29');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (1, 1, 4, 'Trop cool, mais j''ai trop pleuré', '2017-01-05');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (1, 4, 4, 'Un peu cucul', '2021-10-28');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (1, 5, 2, 'Trop de courses poursuites', '2000-02-10');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (11, 5, 5, 'Juste parfait, drole, puissant', '2005-11-03');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (11, 6, 3, 'Sympa, pour tous les ages', '2011-05-10');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (12, 6, 5, 'J''adooore Raiponce', '2012-08-12');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (13, 4, 5, 'Historique et féerique, une régal', '2021-10-13');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (13, 1, 2, 'Beaucoup trop d''eau', '2007-03-06');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (13, 2, 4, 'Drole, sensible, que du bonheur', '2012-10-03');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (15, 3, 5, 'Magnifique, donne envie de danser aussi !!', '2000-07-03');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (16, 6, 2, 'Franchement bof, je préfère la Reine des neiges', '2012-03-03');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (16, 5, 5, 'Ahah trop biiiien', '2009-07-10');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (18, 2, 4, 'Génial mais j''ai peur de faire de l eau maintenant', '2024-01-02');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (21, 4, 5, 'Epoustouflant', '2021-10-17');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (21, 6, 1, 'Je n''aime pas du tout la sorcière, elle fait peur', '2014-01-04');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (8, 5, 4, 'Vraiment trop fan de ce movie, allez tous le voir ou re-voir', '2016-04-28');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (8, 3, 3, 'Borf un peu nian nian mais sympa quand même', '2017-08-04');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (9, 3, 5, 'Trop génial, surtout avec un bon pot de glace !!', '2018-05-04');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (10, 6, 3, 'Yes yes yes, pour petits et grands', '2016-04-04');
+INSERT INTO review (user_id, movie_id, rate, text, review_date)
+VALUES (10, 2, 4, 'Je l''ai vu et re-vu et j''ai toujours adoré', '2023-12-03');
