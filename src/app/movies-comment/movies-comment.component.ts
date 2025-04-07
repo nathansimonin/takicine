5f87eb92bdcb1b1f1dbef0ab1b9e5e552cf39733
nathan.simonin <nathan.simonin@epita.fr>
2025-04-07 10:50:47 +0200

    feat: add moviesComment component

---
+++ src/app/movies-comment/movies-comment.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-comment',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-comment.component.html',
+  styleUrl: './movies-comment.component.scss'
+})
+export class MoviesCommentComponent {
+
+}
