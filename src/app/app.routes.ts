6f1d8b783c36afb42afef0826fd9c74abf8c4d86
nathan.simonin <nathan.simonin@epita.fr>
2025-04-07 10:43:21 +0200

    feat: add componeent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { HomeComponent } from './home/home.component';
 import { MoviesComponent } from './movies/movies.component';
 import { AddMovieComponent } from './movies/add-movie/add-movie.component';
 import { UpdateMovieComponent } from './movies/update-movie/update-movie.component';
+import { MoviesDetailsComponent } from './movies-details/movies-details.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'movies', component: MoviesComponent},
+    { path: 'movies/:id', component: MoviesDetailsComponent},
     { path: 'add-movie', component: AddMovieComponent},
     { path: 'update-movie/:id', component: UpdateMovieComponent}
 ];
---
+++ src/app/movies-details/movies-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-movies-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './movies-details.component.html',
+  styleUrl: './movies-details.component.scss'
+})
+export class MoviesDetailsComponent {
+
+}
