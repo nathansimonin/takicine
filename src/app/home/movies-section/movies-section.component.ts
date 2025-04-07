333c2e6e1e28712df4a3814e97807661ba211721
Thomas Martin-Chemla <thomas.martin-chemla@epita.fr>
2025-04-07 12:23:27 +0200

    Feat: added carousel

--- src/app/home/movies-section/movies-section.component.ts
+++ src/app/home/movies-section/movies-section.component.ts
@@ -2,10 +2,12 @@ import { Component, Input } from '@angular/core';
 import { MovieComponent } from '../movie/movie.component';
 import { Movie } from '../../models/movie';
 
+import { CarouselModule } from 'primeng/carousel'
+
 @Component({
   selector: 'app-movies-section',
   standalone: true,
-  imports: [MovieComponent],
+  imports: [MovieComponent, CarouselModule],
   templateUrl: './movies-section.component.html',
   styleUrl: './movies-section.component.scss'
 })
